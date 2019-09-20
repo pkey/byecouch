914496ebbd7828e2bdee717a94e03bbb05c7db27
Edvinas Beniušis <gashanas@gmail.com>
2019-09-20 23:18:17 +0300

    feat(react): added router

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -3,8 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {
+    Route,
+    NavLink,
+    BrowserRouter as Router,
+    Switch
+} from 'react-router-dom';
+import Calendar from "./views/Calendar";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <NavLink exact activeClassName="active" to="/">
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink activeClassName="active" to="/Calendar">
+                            Calendar
+                        </NavLink>
+                    </li>
+                </ul>
+                <hr />
+                <Switch>
+                    <Route exact path="/" component={App} />
+                    <Route path="/calendar" component={Calendar} />
+                </Switch>
+            </div>
+        </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ packages/client/src/views/Calendar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+class Calendar extends React.Component {
+    render() {
+        return <h1>Calendar</h1>
+    }
+}
+export default Calendar
