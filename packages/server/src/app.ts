b0dd72c4e76bc96f151e4a85aee34eb0e3eef7ae
Andrius Ragelis <andriusragelis@Andriuss-MacBook-Pro.local>
2019-09-20 23:14:45 +0300

    Added dummy endpoint

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -2,6 +2,13 @@ import express from "express";
 const app = express();
 const port = 4000;
 
+var list = [
+    {  name: 'activity1', description: 'some cool activity', address: 'address1' },
+    {  name: 'activity2', description: 'some cool activity too', address: 'address2' }
+];
+
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/activities/", (req, res) => res.send(list));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
