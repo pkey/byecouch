f7ccec84850737b0af770aed423920448290813c
Edvinas Beniušis <gashanas@gmail.com>
2019-09-21 22:22:32 +0300

    feat(react): navigate to activity on marker click

--- packages/client/src/components/Marker.tsx
+++ packages/client/src/components/Marker.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import '../assests/css/Marker.css'
 import { Tooltip } from "antd";
+import { NavLink } from "react-router-dom";
 
 const Marker = (props: any) => {
     const { color, name, id } = props;
     return (
         <Tooltip title={name}>
+            <NavLink to={`activity/${id}`}>
             <div className="marker"
                  style={{ backgroundColor: color, cursor: 'pointer'}}
                  title={name}>
             </div>
+            </NavLink>
         </Tooltip>
 );
 };
--- packages/client/src/views/Map.tsx
+++ packages/client/src/views/Map.tsx
@@ -23,7 +23,7 @@ class ActivityMap extends Component<MapProps> {
         const displayedMarkers = [];
 
         for(const value of this.props.markers? this.props.markers : []) {
-            displayedMarkers.push(<Marker key={value.id} lat={value.lat} lng={value.lng} name={value.activityTitle}/>);
+            displayedMarkers.push(<Marker id={value.id} key={value.id} lat={value.lat} lng={value.lng} name={value.activityTitle}/>);
         }
 
         return (
