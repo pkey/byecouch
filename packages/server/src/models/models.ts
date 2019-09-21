879550ead467bc86a65b66693ab5139c74b0d238
Saulius Tumėnas <tumenas.s@gmail.com>
2019-09-21 17:05:19 +0300

    Fetch activities and spots

--- packages/server/src/models/models.ts
+++ packages/server/src/models/models.ts
@@ -1,16 +1,34 @@
 export interface Spot {
+  id: string,
   name: string;
   address: string;
   latitude: number;
   longitude: number;
+  link: string;
 }
+
 export interface Activity {
-  name: string;
+  link: string;
+  phoneNumber: string;
+  type: ActivityType;
+  events: ActivityEvent[];
+  email: string;
   spot: Spot;
-  description: string;
-}
-export interface Location {
   name: string;
-  latitude: number;
-  longitude: number;
 }
+
+export interface ActivityType {
+  name: string
+}
+
+export interface ActivityEvent {
+  start: string
+  end: string
+}
+
+export interface Location {
+  name: string,
+  latitude: string,
+  longitude: string
+}
+
